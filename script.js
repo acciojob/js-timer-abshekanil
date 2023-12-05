0a64eb8f896c2d39160184b39fce4b7114ad9d51
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2023-12-05 21:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // write js code here if required
+function updateTimer() {
+      const currentDate = new Date();
+      const formattedDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}, ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+      document.getElementById('timer').innerText = formattedDate;
+    }
+
+    // Call the updateTimer function initially
+    updateTimer();
+
+    // Update the timer every second (1000 milliseconds)
+    setInterval(updateTimer, 1000);
